9935083118d58a810fcf140e2d2756b2dc4cfb17
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2018-12-28 04:09:31 +0700

    CRUD for Category

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -21,16 +21,6 @@ class QuestionController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -40,7 +30,7 @@ class QuestionController extends Controller
     {
         // auth()->user()->question()->create($request->all());
         Question::create($request->all());
-        return reponse('Created', Response::HTTP_CREATED);
+        return response('Created', Response::HTTP_CREATED);
     }
 
     /**
@@ -55,17 +45,6 @@ class QuestionController extends Controller
     }
 
     /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Question  $question
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Question $question)
-    {
-        //
-    }
-
-    /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -74,7 +53,8 @@ class QuestionController extends Controller
      */
     public function update(Request $request, Question $question)
     {
-        //
+        $question->update($request->all());
+        return response('Updated', Response::HTTP_ACCEPTED);
     }
 
     /**
